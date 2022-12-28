b557139fb6f2daeb7ccf85f64a322f4692180936
NikPeg <peganov.nik@gmail.com>
2022-12-28 10:03:57 +0300

    New address

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const env = require('env-get');
 const Web3 = require('web3')
 var web3 = new Web3(new Web3.providers.HttpProvider(f"https://goerli.infura.io/v3/{env.get('PRIVATE_KEY')}"));
-const address = '0x707c1343Af50B44889d692b6f7540e86df81f383';
+const address = '0xd9145CCE52D386f254917e481eB44e9943F39138';
 const ABI = env.get('ABI_VALS');
-const MY_ADRESS = '0xa89079d2280b5f9b4c05bc9421f2f00689f2f9b517b588584a439b9030b7c5b4'
+const MY_ADRESS = '0x03AB6A1EdAe3756cd03da838386d5059a13E334B'
 web3.eth.getBalance
 const myContract = new web3.eth.Contract(ABI, address, from {MY_ADRESS})
 
@@ -11,5 +11,3 @@ myContract.methods.transfer().call().then(console.log)
 
 let data = myContract.metods.transfer(MY_ADRESS, web3.utils.toHex(0.0000011)).encodeABI()
 console.log(data);
-
-# Не успел дотестить и додебажить к сожалению :(
